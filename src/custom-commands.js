9a4855687f3900f1b8f3ac3d5c72dba7856d42b3
Chaarizard <aananth2001@gmail.com>
2014-04-11 17:18:33 +0530

    /RESTART

--- src/custom-commands.js
+++ src/custom-commands.js
@@ -2,6 +2,26 @@ const MAX_REASON_LENGTH = 300;
 
 var cmds = {
 	
+	restart: function(target, room, user) {
+		if (!this.can('lockdown')) return false;
+		try {
+			var forever = require('forever'); 
+		} catch(e) {
+			return this.sendReply('/restart requires the "forever" module.');
+		}
+
+		if (!Rooms.global.lockdown) {
+			return this.sendReply('For safety reasons, /restart can only be used during lockdown.');
+		}
+
+		if (CommandParser.updateServerLock) {
+			return this.sendReply('Wait for /updateserver to finish before using /restart.');
+		}
+		this.logModCommand(user.name + ' used /restart');
+		Rooms.global.send('|refresh|');
+		forever.restart('app.js');
+	},
+	
 	reminders: 'reminder',
 	reminder: function(target, room, user) {
 		if (room.type !== 'chat') return this.sendReply("This command can only be used in chatrooms.");
