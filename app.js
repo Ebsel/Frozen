663e3586765924872c8d120b7173cbd7e32044ff
Chaarizard <aananth2001@gmail.com>
2014-04-11 17:24:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -434,3 +434,28 @@ fs.readFile('./config/ipbans.txt', function (err, data) {
 	}
 	Users.checkRangeBanned = Cidr.checker(rangebans);
 });
+try {
+	global.hangman = require('./src/hangman.js').hangman();
+} catch (e) {
+	console.log('Error loading hangman.js');
+}
+try {
+	global.economy = require('./src/money.js');
+} catch (e) {
+	console.log('Error loading economy.js');
+}
+try {
+	global.profile = require('./src/profile.js');
+} catch (e) {
+	console.log('Error loading profile.js');
+}
+try {
+	global.customcommands = require('./src/custom-commands.js');
+} catch (e) {
+	console.log('Error loading custom-commands.js');
+}
+try {
+	global.trainercards = require('./src/trainer-cards.js');
+} catch (e) {
+	console.log('Error loading trainer-cards.js');
+}
